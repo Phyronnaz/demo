4520da9beed120cabb58e13c11d574e7aa6faac2
Phyronnaz <phyronnaz@gmail.com>
2017-12-07 16:10:13 +0100

    Update

--- demo.sh
+++ demo.sh
@@ -1,6 +1,6 @@
 cd Raytracing-demo
 make && ./main
-cd ../Raytracing-demo_specular
-make && ./main
 cd ../Raytracing-Demo_shadows_overlapping
+make && ./main
+cd ../Raytracing-demo_specular
 make && ./main
