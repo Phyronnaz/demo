4b45def98d0f88330e5600ef969a16fce9c4219f
Phyronnaz <phyronnaz@gmail.com>
2017-12-11 11:26:30 +0100

    sh

--- demo.sh
+++ demo.sh
@@ -1,6 +1,21 @@
 cd Raytracing-demo
 make && ./main
+
 cd ../Raytracing-Demo_shadows_overlapping
 make && ./main
+
 cd ../Raytracing-demo_specular
+make && ./main
+
+cd ../Raytracing-reflex
+make && ./main
+
+cd ../Raytracing-cubes
+make && ./main
+
+cd ../Raytracing-floor
+make && ./main
+
+cd ../Raytracing_final_demo
+doxygen Doxyfile
 make && ./main
